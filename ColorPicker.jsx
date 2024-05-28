599cb60e7681b4bfb0089e8f03fd264f5df5959c
Akshaysgmerndeveloper <akshay8310814980@gmail.com>
2024-05-28 13:31:43 +0530

    Add files via upload

--- ColorPicker.jsx
+++ ColorPicker.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 function ColorPicker(){
      
-     const[color, setColor] = useState("#FFFFFFF");
+     const[color, setColor] = useState("#FFFFFF");
 
      function handleColorChange(event){
             setColor(event.target.value);
@@ -12,9 +12,9 @@ function ColorPicker(){
             <div className="color-display" style={{backgroundColor:color}}>
                 <p>Selected Color:{color}</p>
       </div>
-      <label>Select a color:</label>
+      <label>Select a Color:</label>
       <input type="color" value={color} onChange={handleColorChange}/>
      </div>);
 
 }
-export default ColorPicker
+export default ColorPicker;
